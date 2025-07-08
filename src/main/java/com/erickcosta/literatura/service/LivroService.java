0d854a728e449a7720fc8c96c2c46ce0c952061d
Erick Costa <erick.lawliet@gmail.com>
2025-07-08 17:16:32 -0300

    feat: configurar RestTemplate como Bean

---
+++ src/main/java/com/erickcosta/literatura/service/LivroService.java
@@ -0,0 +1,43 @@
+package com.erickcosta.literatura.service;
+
+
+import com.erickcosta.literatura.dto.GutendexBook;
+import com.erickcosta.literatura.dto.GutendexResponse;
+import com.erickcosta.literatura.model.Livro;
+import com.erickcosta.literatura.repository.LivroRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class LivroService {
+
+    @Autowired
+    private LivroRepository livroRepository;
+    @Autowired
+    private RestTemplate restTemplate;
+    private final String URL_API = "https://gutendex.com/books/?search=";
+
+    public List<Livro> buscarSalvarLivros(String termo) {
+        String url = URL_API + termo;
+
+        GutendexResponse response = restTemplate.getForObject(url, GutendexResponse.class);
+        List<Livro> livros = new ArrayList<>();
+
+        if (response != null && response.getResults() != null) {
+            for (GutendexBook b : response.getResults()) {
+                String autor = b.getAuthors().isEmpty() ? "Desconhecido" : b.getAuthors().get(0).getName();
+                String idioma = b.getLanguages().isEmpty() ? "N/A" : b.getLanguages().get(0);
+                Integer downloads = b.getDownload_count() != null ? b.getDownload_count() : 0;
+
+                Livro livro = new Livro(b.getTitle(), autor, idioma, downloads);
+                livros.add(livro);
+                livroRepository.save(livro);
+            }
+        }
+        return livros;
+    }
+}
