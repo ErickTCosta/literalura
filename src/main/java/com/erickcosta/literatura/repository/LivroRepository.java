9759e378b08246b85ed3b382ddce2257f98971d8
Erick Costa <erick.lawliet@gmail.com>
2025-07-11 12:26:58 -0300

    feat: criar método no repository para buscar livros por idioma

--- src/main/java/com/erickcosta/literatura/repository/LivroRepository.java
+++ src/main/java/com/erickcosta/literatura/repository/LivroRepository.java
@@ -3,5 +3,8 @@ package com.erickcosta.literatura.repository;
 import com.erickcosta.literatura.model.Livro;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface LivroRepository extends JpaRepository<Livro, Long> {
+    List<Livro> findByIdioma(String idioma);
 }
